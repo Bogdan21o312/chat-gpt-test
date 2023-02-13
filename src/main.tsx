2ec908ea637c8635ab7cb1fac969641c66a5c6fa
Bogdan21o312 <lutskiv2010@gmail.com>
2023-02-14 00:56:19 +0200

    Updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import '@testing-library/jest-dom/extend-expect';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
